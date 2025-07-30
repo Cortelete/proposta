1ea41f2be3b05684f32fd4ede7fcf2a8f4f2f05c
Cortelete <d.cortelete@gmail.com>
2025-07-30 03:21:12 -0300

    alteração logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -115,7 +115,7 @@ const Header: React.FC<HeaderProps> = ({ theme, toggleTheme, onWhatsAppClick }) 
       >
         <div className="absolute inset-0 bg-black/60 backdrop-blur-sm" onClick={() => setIsMenuOpen(false)}></div>
         
-        <div className={`relative z-50 w-full max-w-xs h-full bg-[var(--card-bg)] border-r border-[var(--border-color)] shadow-2xl flex flex-col transition-transform duration-300 ease-in-out ${isMenuOpen ? 'transform translate-x-0' : 'transform -translate-x-full'}`}>
+        <div className={`relative z-50 w-full max-w-xs h-24 bg-[var(--card-bg)] border-r border-[var(--border-color)] shadow-2xl flex flex-col transition-transform duration-300 ease-in-out ${isMenuOpen ? 'transform translate-x-0' : 'transform -translate-x-full'}`}>
           <div className="flex items-center justify-between p-6 border-b border-[var(--border-color)]">
               <img src="/images/logoIA.png" alt="InteligenciArte.IA Logo" className="h-12 w-auto" />
               <button
